becafae4b258354902b67b5c85a4ea5c74d65e00
Stefano Mandelli <linuxfree2@gmail.com>
2019-05-12 22:47:58 +0200

    Created the docstring of `genpoints.jl`

--- src/genpoints.jl
+++ src/genpoints.jl
@@ -1,5 +1,26 @@
+# Some fft routine like fftfreq
 include("fft_util.jl")
 
+
+"""
+    function get_points(N_pix::Int, sky::Array{Float64, 2}; direction::Bool)
+# Brief Description
+This function return the pointing in accord to the sky scanning-strategy.
+Now return the pointing directions as pixels. You can choose between two
+different scanning strategy mode: if `direction = true` the function return a
+left-right scanning. If `direction = false` the return is a up-down scanning.
+
+Future improvements: I ought code a new`get_points` function in order to
+get true sky-direction in Right-ascension and Declination coordinates
+
+# Development
+The function as is, has not presented weird behaviours.
+
+# Args description
+N_pix:      Number of pixel, in linear coordinate
+sky  :      The sky that you want observe, in Array{Float64, 2} format
+direction:  If true = Left-Right scanning else Up-Down.
+"""
 function get_points(N_pix::Int, sky::Array{Float64, 2}; direction::Bool)
 
     points = Array{Float64, 1}(undef, 0)
@@ -51,6 +72,21 @@ function get_points(N_pix::Int, sky::Array{Float64, 2}; direction::Bool)
 end
 
 
+"""
+    function get_tod(sky::Array{Float64, 2}, points::Array{Int64, 2})
+
+# Brief description
+Given a set of points, genereted either L-R or U-D scanning, return the
+time order data (TOD) series detected by a sensors.
+
+# Development
+The function is very simple and has not shown weird behaviours.
+
+# Args description
+sky  :      The sky that you want observe, in Array{Float64, 2} format
+points:     Time-stream of pointings of your sky scanning-strategy.
+
+"""
 function get_tod(sky::Array{Float64, 2}, points::Array{Int64, 2})
 
     Num_of_pixels = size(points)[1]
@@ -61,6 +97,21 @@ function get_tod(sky::Array{Float64, 2}, points::Array{Int64, 2})
 end
 
 
+"""
+    function observe_sky(N_pix::Int, sky::Array{Float64, 2};
+                         noise::Bool, direction_l_r::Bool)
+# Brief description
+This function return a noisy tod. The noise is a superimposition of a
+one-over-f like: include the instrumental fluctuation and atmospheric
+noise, and by a second part of white noise
+
+# Args description
+N_pix:      Number of pixel, in linear coordinate
+sky  :      The sky that you want observe, in Array{Float64, 2} format
+direction:  If true = Left-Right scanning else Up-Down.
+noise :     Is a useless args ... I ought clean this mess!
+
+"""
 function observe_sky(N_pix::Int, sky::Array{Float64, 2};
                      noise::Bool, direction_l_r::Bool)
 
@@ -90,14 +141,28 @@ function tod_noise(Nsamp::Int, dt::Float64, fknee::Float64, alpha::Float64,
 end
 
 
-function binned_map(N::Int, tod::Array{Float64, 1}, pointing::Array{Int64, 2})
-    map = zeros(N, N)
-    hit = zeros(N, N)
+"""
+    function binned_map(N::Int, tod::Array{Float64, 1},
+                        pointing::Array{Int64, 2})
+# Brief description
+The function returns the baselines sum, named Sky Binned Map. If P is the
+pointing matrix, returns: PᵀP
+
+# Args description
+N_pix:      Number of pixel, in linear coordinate
+tod  :      Time Order Data stream, returned by get_tod function
+points:     Time-stream of pointings of your sky scanning-strategy.
+"""
+function binned_map(N_pix::Int, tod::Array{Float64, 1},
+                    points::Array{Int64, 2})
+    map = zeros(N_pix, N_pix)
+    hit = zeros(N_pix, N_pix)
 
     for i = 1:length(tod)
-        map[pointing[i, 1], pointing[i, 2]] += tod[i]
-        hit[pointing[i, 1], pointing[i, 2]] += 1.0
+        map[points[i, 1], points[i, 2]] += tod[i]
+        hit[points[i, 1], points[i, 2]] += 1.0
     end
 
     return map, hit
+
 end
